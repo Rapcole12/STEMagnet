628ed2ecb2f94fa1c16108933ea478d0e4891524
Elijah <elijah.widenerferreira@gmail.com>
2024-02-03 13:12:38 -0500

    Created files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
 
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import Home from './pages/Home';
+import profileSettuo from './pages/profileSettuo.jsx'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return (<BrowserRouter><Routes>
+          <Route path = '/' element =
+           {
+             <Home />
+           } />
+        <Route path="/SignIn
+             ' element={<SignIn />} /' < Route path = '/SignUp' element =
+           {
+             <SignUp />
+           } />
+        <Route path="/profileSettup ' element={<settUp />} /'
+
+               < /Routes>
+    </BrowserRouter>);
 }
 
 export default App;
