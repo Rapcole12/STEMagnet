88e74451062dbe38055f92b94a099d7ef9854fde
Rapcole12 <rapcole12@vt.edu>
2024-02-03 14:29:10 -0500

    Send Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import FindingMatchesPage from "./pages/findingMatchesPage"
+import Inbox from "./pages/inbox"
+import MenteeProfile from "./pages/menteeProfile"
+import MentorProfile from "./pages/mentorProfile"
+import ProfileSettup from "./pages/profileSettup"
+import Quotes from "./pages/quotes"
+
 
 function App() {
   return (
@@ -12,6 +19,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/FindingMatchesPage" element={<FindingMatchesPage />} />
+        <Route path="/Inbox" element={<Inbox />} />
+        <Route path="/MenteeProfile" element={<MenteeProfile />} />
+        <Route path="/MentorProfile" element={<MentorProfile />} />
+        <Route path="/ProfileSettup" element={<ProfileSettup />} />
+        <Route path="/Quotes" element={<Quotes />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/Nav'
+
+function Home() {
+  return (
+    <main>
+        <div>
+            <NavBar />
+        </div>
+    </main>
+  )
+}
+
+export default Home
