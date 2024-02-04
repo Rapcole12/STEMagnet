a38f65164b02b0476fe7dcc7480532484aa7c4e1
riker <rikerwilson@vt.edu>
2024-02-03 22:28:01 -0500

    fixed margins for aboutus

--- frontend/src/pages/aboutUs.jsx
+++ frontend/src/pages/aboutUs.jsx
@@ -4,17 +4,18 @@ import StemMagnetTransparent from "../assets/stemagentTransparent.png";
 
 function AboutUs() {
   return (
-    <main className="h-screen overflow-y-hidden">
+    <main className="h-screen">
       <div className="sticky top-0 border-b border-black">
         <NavBar />
       </div>
-      <div className="h-screen flex justify-center items-center bg-gradient-to-r from-purple-400 to-violet-800">
-      <div className="bg-white rounded-lg p-12 absolute left-12 right-12 shadow-lg">
-        <h1 className="text-5xl text-[#333333] font-semibold mb-4 ml-8 mt-5 ">ABOUT US</h1>
+      <div className="h-screen bg-gradient-to-r from-purple-400 to-violet-800 overflow-hidden">
+        <div className="h-screen flex justify-center items-center relative">
+          <div className="bg-white rounded-lg p-12 absolute left-12 right-12 top-20 bottom-12 overflow-auto shadow-lg">
+            <h1 className="text-5xl text-[#333333] font-semibold mb-4 ml-8 mt-5">ABOUT US</h1>
 
-          <div className="ml-8 mt-5">
-            <h2 className="text-[22px] font-semibold text-[#333333]">What is STEMAGENT?</h2>
-            <p className="text-[18px] text-[#666666]">
+            <div className="ml-8 mt-5">
+              <h2 className="text-[22px] font-semibold text-[#333333]">What is STEMAGENT?</h2>
+              <p className="text-[18px] text-[#666666]">
               STEMAGNET is a website dedicated to promoting STEM (Science, Technology, Engineering, and Mathematics) education and empowering the next generation of innovators and problem solvers. Through our innovative swiping technique, mentees and mentors can connect faster than ever! Mentors, who are experienced experts in their respective fields, can choose mentees to guide and share their knowledge with. On the other hand, mentees have the opportunity to swipe and choose mentors who align with their interests and goals. STEMAGNET aims to facilitate mentorship relationships and provide a supportive community for individuals passionate about STEM, ultimately fostering skill development and inspiring future achievements in science and technology.
             </p>
           </div>
@@ -37,6 +38,7 @@ function AboutUs() {
             </p>
           </div>
         </div>
+      </div>
       </div>
     </main>
   );
